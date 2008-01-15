39b3722469a0f765c52c202b4fcb3bc7502a5007
mrjtuk <mrjtuk@05275033-79c2-2956-22f4-0a99e774df92>
2008-01-15 12:44:19 +0000

    refactor of Docking package

--- bazaar/DockTest/DockTest.h
+++ bazaar/DockTest/DockTest.h
@@ -15,12 +15,15 @@ class DockTest : public WithDockTestLayout< DockWindow > {
 public:
 	typedef DockTest CLASSNAME;
 	
-	WithTestLayout1<DockableCtrl> docker1;
-	DockableCtrl docker2, docker3, docker4, docker5;
+	// DockableCtrls
+	WithToolsLayout<DockableCtrl> docker1;
+	WithDockOptionsLayout<DockableCtrl> docker2;
+	DockableCtrl docker3, docker4, docker5;
+	// Child ctrls for DockableCtrl
 	MenuBar menu;
 	ArrayCtrl array1;
 	ToolBar	tool1;
-	TreeCtrl tree1, tree2;
+	TreeCtrl tree1;
 	Button btns[BUTTON_COUNT];
 	
 	DockTest() {
@@ -33,25 +36,51 @@ public:
 		FillToolBar();
 		// Layout and add controls to docker windows
 		CtrlLayout(docker1, "Tool Window");
+
+		CtrlLayout(docker2, "Dock Options");
+		docker2.SetStyle(DockableCtrl::StyleDefaultVert());
+		docker2.dockleft 	<<= THISBACK1(OnCtrlDockAllow, 0);		
+		docker2.docktop 	<<= THISBACK1(OnCtrlDockAllow, 1);		
+		docker2.dockright 	<<= THISBACK1(OnCtrlDockAllow, 2);		
+		docker2.dockbottom 	<<= THISBACK1(OnCtrlDockAllow, 3);		
+		docker2.verthandle	<<= THISBACK(OnVertHandle);
+		docker2.dockleft 	<<= true;
+		docker2.docktop 	<<= true;
+		docker2.dockright 	<<= true;
+		docker2.dockbottom 	<<= true;
+		docker2.verthandle 	<<= docker2.GetStyle().handle_vert;
+
 		docker1.Icon(CtrlImg::print());
-		docker2.Icon(CtrlImg::Home()).Add(array1.SizePos());
-		docker3.Icon(CtrlImg::cut()).Add(tree1.SizePos());
-		docker4.Icon(CtrlImg::copy()).Add(tree2.SizePos());
+		docker3.Icon(CtrlImg::Home()).Add(array1.SizePos());
+		docker4.Icon(CtrlImg::cut()).Add(tree1.SizePos());
 		docker5.Icon(CtrlImg::save()).Add(tool1.SizePos());
 		
-		Dock(DOCK_TOP, docker2.SizeHint(Size(100, 100)), "ArrayCtrl");
-		Dock(DOCK_LEFT, docker3.SizeHint(Size(80, 200)), "TreeCtrl");
-		Dock(DOCK_RIGHT, docker4.SizeHint(Size(80, 200)), "TreeCtrl");
-		Dock(DOCK_BOTTOM, docker5.SizeHint(Size(90, 22)), "ToolBar");
-	
+		Dock(DOCK_TOP, docker3.SizeHint(Size(100, 100)), "ArrayCtrl");
+		Dock(DOCK_LEFT, docker4.SizeHint(Size(80, 200)), "TreeCtrl");
+		Dock(DOCK_LEFT, docker2);
+		Dock(DOCK_BOTTOM, docker5.SizeHint(Size(90, 50)), "ToolBar");
 		docker3.AddFrame(menu);
 	
 		menu.Set(THISBACK(OnMenu));
 	
-		simple <<= THISBACK(OnSimple);
-		tabbing <<= THISBACK(OnTabbing);
-		simple <<= IsSimple();
-		tabbing <<= IsTabbing();
+		// Set Option callbacks
+		simple 		<<= THISBACK(OnSimple);
+		tabbing 	<<= THISBACK(OnTabbing);
+		autohide	<<= THISBACK(OnAutoHide);
+		animate 	<<= THISBACK(OnAnimate);
+		dockleft 	<<= THISBACK1(OnMainDockAllow, 0);		
+		docktop 	<<= THISBACK1(OnMainDockAllow, 1);		
+		dockright 	<<= THISBACK1(OnMainDockAllow, 2);		
+		dockbottom 	<<= THISBACK1(OnMainDockAllow, 3);		
+		// Set Option data
+		simple 		<<= IsSimple();
+		tabbing 	<<= IsTabbing();
+		autohide 	<<= IsAutoHide();
+		animate 	<<= IsAnimated();
+		dockleft 	<<= true;
+		docktop 	<<= true;
+		dockright 	<<= true;
+		dockbottom 	<<= true;
 //		windows <<= THISBACK(ShowWindowStatus);
 	}
 	
@@ -60,20 +89,59 @@ public:
 		DockWindow::State(reason);	
 		if (reason == Ctrl::OPEN) {
 			ASSERT(IsOpen());
-			Float(docker1);	
+#if defined(PLATFORM_WIN32) 
+			Float(docker1); // This currently doesn't work on X11.
+#endif
 		}
 	}
 	
-	void OnSimple()
-	{
-		SimpleMode(~simple);
-	}
-
-	void OnTabbing()
-	{
-		Tabbing(~tabbing);
-	}
+	void OnSimple()  { SimpleMode(~simple); }
+	void OnTabbing() { Tabbing(~tabbing); }	
+	void OnAutoHide() { AutoHide(~autohide); }	
+	void OnAnimate() { Animate(~animate); }
 	
+	void OnMainDockAllow(int align)
+	{
+		switch (align) {
+		case DOCK_LEFT:
+			AllowDockLeft(~dockleft);
+			return;
+		case DOCK_TOP:
+			AllowDockTop(~docktop);
+			return;
+		case DOCK_RIGHT:
+			AllowDockRight(~dockright);
+			return;
+		case DOCK_BOTTOM:
+			AllowDockBottom(~dockbottom);
+			return;
+		}
+	}
+		
+	void OnCtrlDockAllow(int align)
+	{
+		switch (align) {
+		case DOCK_LEFT:
+			docker2.AllowDockLeft(~docker2.dockleft);
+			return;
+		case DOCK_TOP:
+			docker2.AllowDockTop(~docker2.docktop);
+			return;
+		case DOCK_RIGHT:
+			docker2.AllowDockRight(~docker2.dockright);
+			return;
+		case DOCK_BOTTOM:
+			docker2.AllowDockBottom(~docker2.dockbottom);
+			return;
+		}
+	}		
+		
+	void OnVertHandle()
+	{
+		bool v = ~docker2.verthandle;
+		docker2.SetStyle(v ? DockableCtrl::StyleDefaultVert() : DockableCtrl::StyleDefault());
+	}
+		
 	void OnMenu(Bar &bar)
 	{
 		bar.Add("Menu1", THISBACK(OnTopMenu));
@@ -100,7 +168,6 @@ public:
 		for(int i = 0; i < 200; i++)
 			array1.Add(i, FormatIntRoman(i, true));
 		FillTree(tree1);
-		FillTree(tree2);
 	}
 	
 	void FillTree(TreeCtrl &tree) {

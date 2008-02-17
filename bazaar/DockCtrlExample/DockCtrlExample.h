04fb9e795d39e317b4798df82f94aca99d3fc769
xmaldororx <xmaldororx@05275033-79c2-2956-22f4-0a99e774df92>
2008-02-17 03:16:38 +0000

    DockCtrl 802b.1 (ver. 0.50) Major Update and Example Source code

---
+++ bazaar/DockCtrlExample/DockCtrlExample.h
@@ -0,0 +1,44 @@
+#ifndef _DockCtrlExample_DockCtrlExample_h
+#define _DockCtrlExample_DockCtrlExample_h
+
+#include <DockCtrl/DockCtrl.h>
+#include <RichEdit/RichEdit.h>
+
+using namespace Upp;
+
+#define LAYOUTFILE <DockCtrlExample/DockCtrlExample.lay>
+#include <CtrlCore/lay.h>
+
+
+
+class DockCtrlExample : public WithDockCtrlExampleLayout<TopWindow> {
+public:
+	typedef DockCtrlExample CLASSNAME;
+	DockCtrlExample();
+	
+
+	DockCtrl 	dockctrl;								// Main Widget 		-	acts as a "server" for it's "client" widgets.
+	DockWindow	dock1, dock2, dock3, dock4, dock5;		// Actual Widgets 	-	these are the dockable "client" widgets
+	
+	RichEdit	richedit;								// Stuff to "wrap"	
+	TabCtrl		tabctrl;								//
+	Button		button1, button2, button3;				//
+	ArrayCtrl	array1, array2;							//
+	MenuBar 	menubar;								//
+	ToolBar 	toolbar;								//
+	StatusBar 	statusbar;								//
+
+	// Other methods (not necessary; only for demo purpose...
+
+	void Toolbar	(Bar &bar);
+	void Menubar	(Bar &bar);
+	void WindowsMenu(Bar& bar); 
+	void InfoMenu	(Bar& bar);
+	void EditMenu	(Bar& bar);
+	void Click()	{}
+
+	
+};
+
+#endif
+

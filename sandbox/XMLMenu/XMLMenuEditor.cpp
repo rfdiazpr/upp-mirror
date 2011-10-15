18a5859b5f4f3056ad5c92c6348c15d1f8260183
micio <micio@05275033-79c2-2956-22f4-0a99e774df92>
2011-10-15 10:11:20 +0000

    sandbox/XMLMenu : more progress in menu editor - layout completed

--- sandbox/XMLMenu/XMLMenuEditor.cpp
+++ sandbox/XMLMenu/XMLMenuEditor.cpp
@@ -22,9 +22,44 @@ XMLMenuEditor::XMLMenuEditor(XMLMenuInterface *_iFace) : iFace(_iFace)
 	okButton.Ok() <<= THISBACK(okCb);
 	cancelButton.Cancel() <<= THISBACK(cancelCb);
 	
+	// gets size of item editor pane, we keep that one fixed size
+	itemSize = itemPane.GetLayoutSize();
+	
+	// get command pane size, that one will be fixed too
+	cmdSize = cmdPane.GetLayoutSize();
+	
+	// gets button vertical neede size
+	buttonVertSize = GetLayoutSize().cy - horzSplitter.GetSize().cy;
+	
+	// adds items to horz splitter
+	horzSplitter.Horz(cmdPane, rightPane);
+
+	// adds layout to all ctrls
+	CtrlLayout(cmdPane);
+	CtrlLayout(rightPane);
+	CtrlLayout(tabsPane);
+	CtrlLayout(itemPane);
+	
+	// adjust window width to accomodate at least itemPane and cmdPane
+	minWidth = itemSize.cx + cmdSize.cx;
+	
+	// adjust window height to accomodate cmdPane
+	minHeight = cmdSize.cy;
+	
 	// setup tabs
-	tabCtrl.Add(t_("Menu"));
-	tabCtrl.Add(t_("ToolBars"));
+	tabsPane.tabCtrl.Add(t_("Menu"));
+	tabsPane.tabCtrl.Add(t_("ToolBars"));
+	
+	int w = max(GetSize().cx, minWidth);
+	int h = max(GetSize().cy - buttonVertSize, minHeight);
+	int wr = w;
+	int hr = h + buttonVertSize;
+	SetRect(GetRect().left, GetRect().top, wr, hr);
+	
+	// adds items to vertical splitter
+	rightPane.vertSplitter.Vert(tabsPane, itemPane);
+	
+	Sizeable();
 	
 	// reads the commands and put them into list
 	GetCommands();
@@ -34,14 +69,22 @@ XMLMenuEditor::~XMLMenuEditor()
 {
 }
 
+// adjust layout on win changes
+void XMLMenuEditor::Layout(void)
+{
+	// adjust splitter sizes
+	horzSplitter.SetPos(10000 * cmdSize.cx / GetSize().cx);
+	int netH = GetSize().cy - buttonVertSize;
+	rightPane.vertSplitter.SetPos(10000 * (netH - itemSize.cy) / netH);
+}
+		
 // gets commands from iFace and fills the command list
 void XMLMenuEditor::GetCommands(void)
 {
 	Vector<String> const &ids = iFace->GetCommands().GetIds();
-DLOG(ids.GetCount());
-	commandList.Clear();
+	cmdPane.commandList.Clear();
 	for(int i = 0; i < ids.GetCount(); i++)
-		commandList.Add(ids[i]);
+		cmdPane.commandList.Add(ids[i]);
 }
 	
 END_UPP_NAMESPACE
--- sandbox/XMLMenu/XMLMenuEditor.h
+++ sandbox/XMLMenu/XMLMenuEditor.h
@@ -10,16 +10,40 @@ NAMESPACE_UPP
 #define LAYOUTFILE <XMLMenu/XMLMenuEditor.lay>
 #include <CtrlCore/lay.h>
 
-class XMLMenuEditor : public WithXMLMenuEditorLayout<TopWindow>
+class XMLMenuEditor : public WithMenuEditorLayout<TopWindow>
 {
 	private :
 		// interface to access menu system data
 		XMLMenuInterface *iFace;
+		
+		// left pane
+		WithCmdBoxLayout<ParentCtrl> cmdPane;
+
+		// right pane - contains splitter for tabbed ctrl and layout editor
+		WithTabItemLayout<ParentCtrl> rightPane;
+		
+		// tab ctrl pane
+		WithTabsLayout<ParentCtrl> tabsPane;
+		
+		// item editor
+		WithItemEditorLayout<ParentCtrl> itemPane;
+		
+		// vertical size of button area
+		int buttonVertSize;
+		
+		// original sizes of cmd area and item editor area
+		Size cmdSize, itemSize;
+
+		// minimum window width and height
+		int minWidth, minHeight;
 	
 	protected :
 	
 		void cancelCb(void);
 		void okCb(void);
+		
+		// adjust layout on win changes
+		void Layout(void);
 		
 		// gets commands from iFace and fills the command list
 		void GetCommands(void);

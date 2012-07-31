b7279757019dac83b4d7e41e6476e409be16edc3
cxl <cxl@05275033-79c2-2956-22f4-0a99e774df92>
2012-07-31 20:12:50 +0000

    .sandbox

--- sandbox/Algo/Tree.cpp
+++ sandbox/Algo/Tree.cpp
@@ -5,9 +5,94 @@
 void Inorder(Node *n)
 {
 	if(n) {
+		Inorder(n->left);
+		LOG(n);
+		Inorder(n->right);
+	}
+}
+
+void Inorder(Node *n)
+{
+start:
+	if(n) {
+	start1:
+		SE& x = stack.Add();
+		x.n = n;
+		x.ret = 1;
+		n = n->left;
+		goto start;
+	ret1:
+		LOG(n);
+		SE& x = stack.Add();
+		x.n = n;
+		n = n->right;
+		goto start;
+	ret2:
+	}
+	SE x = stack.Pop();
+	n = x.n;
+	if(x.ret == 1)
+		goto ret1;
+	else
+		goto ret2;
+}
+
+void Inorder(Node *n)
+{
+	
+	switch(ip) {
+	case 0:
+	if(n) {
 		Inorder(~n->left);
+	case 1:
 		LOG(n->value);
 		Inorder(~n->right);
+	case 2:
+	}
+}
+
+
+void Inorder(Node *n)
+{
+	switch(ip) {
+	case 0:
+	if(n) {
+		Inorder(~n->left);
+	case 1:
+		LOG(n->value);
+		Inorder(~n->right);
+	case 2:
+		break;
+	}
+	}
+
+void {
+	struct SE {
+		Node *n;
+		int   next;
+	};
+	
+	Vector<Stack> stack;
+	
+	SE& e = stack.Add();
+	e.n = n;
+	e.next = 0;
+	
+	while(stack) {
+		SE x = stack.Pop();
+		switch(x.next) {
+		case 0:
+			x.next = 1;
+			x.n = ~x.n->left;
+			stack.Add(x);
+			break;
+		case 1:
+			LOG(n->value);
+			x.next = 0;
+			x.n = ~x.n->right;
+			stack.Add(x);
+			break;
+		}
 	}
 }
 

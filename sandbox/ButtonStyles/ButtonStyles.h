2e530c9718c0c29144eb1e3aa35452c6c18c0d72
didier <didier@05275033-79c2-2956-22f4-0a99e774df92>
2013-01-06 11:45:35 +0000

    Added : ButtonStyles   package It adds somme specific styling capabilities to Button class

---
+++ sandbox/ButtonStyles/ButtonStyles.h
@@ -0,0 +1,102 @@
+#ifndef _ButtonStyles_ButtonStyles_h
+#define _ButtonStyles_ButtonStyles_h
+
+#include <CtrlLib/CtrlLib.h>
+
+using namespace Upp;
+
+
+namespace ButtonStyles {
+
+	
+typedef struct {
+	Image buttonIcon; // image à mettre dans le cercle
+	Color opaqueColor; // couleur du cercle au bout du bouton
+	Color alphaColor;
+	int   subbuttonDeflate;
+	int   subbuttonBorderWidth;
+	int   alphaValue;
+	int   flags;
+	enum {
+		LEFT_END_ROUND             = 0x0001,
+		RIGHT_END_ROUND            = 0x0002,
+		CONTAINS_SUBBUTON          = 0x0004,
+		SUBBUTTON_IS_FULL          = 0x0008|CONTAINS_SUBBUTON,
+		SUBBUTTON_LEFT_END_ROUND   = 0x0010|CONTAINS_SUBBUTON,
+		SUBBUTTON_RIGHT_END_ROUND  = 0x0020|CONTAINS_SUBBUTON,
+		SUB_ITEM_LEFT              = 0x0040,
+		SUB_ITEM_RIGHT             = 0x0080,
+		
+		SUBBUTTON_IS_SHORT_LEFT    = SUB_ITEM_LEFT|CONTAINS_SUBBUTON,
+		SUBBUTTON_IS_SHORT_RIGHT   = SUB_ITEM_RIGHT|CONTAINS_SUBBUTON,
+
+		TEXT_USED_AS_MASK          = 0x8000,
+
+		BOTH_ENDS_ROUND            = LEFT_END_ROUND | RIGHT_END_ROUND,
+		BOTH_SUBBUTTON_ENDS_ROUND  = SUBBUTTON_LEFT_END_ROUND | SUBBUTTON_RIGHT_END_ROUND,
+		ALL_ENDS_ROUND             = BOTH_ENDS_ROUND | BOTH_SUBBUTTON_ENDS_ROUND
+	};
+} ButtonLook;
+
+enum {
+	BUTTON_STYLE_0a = 0,
+	BUTTON_STYLE_0b = ButtonLook::SUB_ITEM_LEFT,
+	BUTTON_STYLE_0c = ButtonLook::SUB_ITEM_RIGHT,
+
+	// NO SUBBUTTON ( ICON CENTER )
+	BUTTON_STYLE_1a = ButtonLook::LEFT_END_ROUND,
+	BUTTON_STYLE_1b = ButtonLook::RIGHT_END_ROUND,
+	BUTTON_STYLE_1c = ButtonLook::BOTH_ENDS_ROUND,
+	// NO SUBBUTTON ( ICON LEFT )
+	BUTTON_STYLE_1aL = ButtonLook::LEFT_END_ROUND|ButtonLook::SUB_ITEM_LEFT,
+	BUTTON_STYLE_1bL = ButtonLook::RIGHT_END_ROUND|ButtonLook::SUB_ITEM_LEFT,
+	BUTTON_STYLE_1cL = ButtonLook::BOTH_ENDS_ROUND|ButtonLook::SUB_ITEM_LEFT,
+	// NO SUBBUTTON ( ICON RIGHT )
+	BUTTON_STYLE_1aR = ButtonLook::LEFT_END_ROUND|ButtonLook::SUB_ITEM_RIGHT,
+	BUTTON_STYLE_1bR = ButtonLook::RIGHT_END_ROUND|ButtonLook::SUB_ITEM_RIGHT,
+	BUTTON_STYLE_1cR = ButtonLook::BOTH_ENDS_ROUND|ButtonLook::SUB_ITEM_RIGHT,
+	
+	// FULL  SUBBUTTON
+	BUTTON_STYLE_2a = ButtonLook::LEFT_END_ROUND |ButtonLook::SUBBUTTON_IS_FULL|ButtonLook::SUBBUTTON_LEFT_END_ROUND,
+	BUTTON_STYLE_2b = ButtonLook::RIGHT_END_ROUND|ButtonLook::SUBBUTTON_IS_FULL|ButtonLook::SUBBUTTON_RIGHT_END_ROUND,
+	BUTTON_STYLE_2c = ButtonLook::LEFT_END_ROUND |ButtonLook::SUBBUTTON_IS_FULL|ButtonLook::BOTH_SUBBUTTON_ENDS_ROUND,
+	BUTTON_STYLE_2d = ButtonLook::RIGHT_END_ROUND|ButtonLook::SUBBUTTON_IS_FULL|ButtonLook::BOTH_SUBBUTTON_ENDS_ROUND,
+	BUTTON_STYLE_2e = ButtonLook::LEFT_END_ROUND |ButtonLook::SUBBUTTON_IS_FULL|ButtonLook::SUBBUTTON_LEFT_END_ROUND |ButtonLook::SUBBUTTON_IS_SHORT_LEFT,
+	BUTTON_STYLE_2f = ButtonLook::RIGHT_END_ROUND|ButtonLook::SUBBUTTON_IS_FULL|ButtonLook::SUBBUTTON_RIGHT_END_ROUND|ButtonLook::SUBBUTTON_IS_SHORT_RIGHT,
+	BUTTON_STYLE_2g = ButtonLook::ALL_ENDS_ROUND |ButtonLook::SUBBUTTON_IS_FULL,
+	BUTTON_STYLE_2h = ButtonLook::ALL_ENDS_ROUND |ButtonLook::SUBBUTTON_IS_FULL|ButtonLook::SUBBUTTON_IS_SHORT_LEFT,
+	BUTTON_STYLE_2i = ButtonLook::ALL_ENDS_ROUND |ButtonLook::SUBBUTTON_IS_FULL|ButtonLook::SUBBUTTON_IS_SHORT_RIGHT,
+
+	// EMPTY SUBBUTTON
+	BUTTON_STYLE_3a  = ButtonLook::LEFT_END_ROUND |ButtonLook::SUBBUTTON_LEFT_END_ROUND,
+	BUTTON_STYLE_3b  = ButtonLook::RIGHT_END_ROUND|ButtonLook::SUBBUTTON_RIGHT_END_ROUND,
+	BUTTON_STYLE_3c  = ButtonLook::LEFT_END_ROUND |ButtonLook::BOTH_SUBBUTTON_ENDS_ROUND,
+	BUTTON_STYLE_3d  = ButtonLook::RIGHT_END_ROUND|ButtonLook::BOTH_SUBBUTTON_ENDS_ROUND,
+	BUTTON_STYLE_3e  = ButtonLook::LEFT_END_ROUND |ButtonLook::SUBBUTTON_LEFT_END_ROUND |ButtonLook::SUBBUTTON_IS_SHORT_LEFT,
+	BUTTON_STYLE_3f  = ButtonLook::RIGHT_END_ROUND|ButtonLook::SUBBUTTON_RIGHT_END_ROUND|ButtonLook::SUBBUTTON_IS_SHORT_RIGHT,
+	BUTTON_STYLE_3g  = ButtonLook::ALL_ENDS_ROUND,
+	BUTTON_STYLE_3h  = ButtonLook::ALL_ENDS_ROUND |ButtonLook::SUBBUTTON_IS_SHORT_LEFT,
+	BUTTON_STYLE_3i  = ButtonLook::ALL_ENDS_ROUND |ButtonLook::SUBBUTTON_IS_SHORT_RIGHT,
+
+
+};
+
+
+Value MakeButtonLook( int flags, Color opaqueColor, Color alphaColor, int alphaValue, Image buttonIcon );
+
+
+typedef struct {
+	int stylingFlags;
+	Color textColor;
+	Color opaqueColor;
+	Color alphaColor;
+	int alphaValue;
+	Image buttonIcon;
+} StyledButtonStyle;
+
+
+}
+
+
+
+#endif
---
+++ sandbox/ButtonStyles/ButtonsStyles.cpp
@@ -0,0 +1,159 @@
+#include "ButtonStyles.h"
+
+#include <Draw/Draw.h>
+#include <Painter/Painter.h>
+
+namespace ButtonStyles {
+
+Value MakeButtonLook( int flags, Color opaqueColor, Color alphaColor, int alphaValue, Image buttonIcon )
+{
+	ButtonLook e;
+	e.buttonIcon = buttonIcon;
+	e.opaqueColor = opaqueColor;
+	e.alphaColor = alphaColor;
+	e.alphaValue = alphaValue;
+	e.flags = flags;
+	e.subbuttonDeflate = 6;
+	e.subbuttonBorderWidth = 4;
+	return RawToValue(e);
+}
+
+#define TEST(FLAG)  ((e.flags & ButtonLook::FLAG)==ButtonLook::FLAG)
+
+Value ButtonStyleLookFunction(Draw& dw, const Rect& rect, const Value& v, int op)
+{
+	if( IsTypeRaw<ButtonLook>(v) )
+	{
+
+		const ButtonLook& e = ValueTo<ButtonLook>(v);
+		switch(op)
+		{
+			case LOOK_MARGINS:
+				return Rect(2,2,2,2);
+			case LOOK_ISOPAQUE:
+				return false;
+			case LOOK_PAINT:
+			{
+				Rect LeftCircleRect( rect.TopLeft(), Size(rect.Height(), rect.Height()) );
+				Rect RightCircleRect = LeftCircleRect;
+				RightCircleRect.OffsetHorz(rect.Width()-rect.Height());
+				Rect bandRect = rect;
+				ImageDraw iw( rect.Width(), rect.Height() );
+
+				if (TEST(LEFT_END_ROUND))
+				{
+					bandRect.left = bandRect.left+bandRect.Height()/2;
+					iw.Alpha().DrawEllipse( LeftCircleRect, GrayColor(e.alphaValue));
+				}
+				if (TEST(RIGHT_END_ROUND))
+				{
+					bandRect.right = bandRect.right-bandRect.Height()/2;
+					iw.Alpha().DrawEllipse( RightCircleRect, GrayColor(e.alphaValue));
+				}
+				bandRect.Deflate(0,1); // to have same size as circles
+				iw.Alpha().DrawRect(bandRect, GrayColor(e.alphaValue));
+				dw.DrawImage( rect, iw , e.alphaColor);
+
+				#if DO_ANTIALIASING
+				ImageBuffer ib(rect.Size());
+				BufferPainter w(ib, MODE_ANTIALIASED);
+				#else
+				#define w dw
+				#endif
+
+
+				bandRect=rect;
+				bandRect.Deflate(0, e.subbuttonDeflate+1);
+				LeftCircleRect.Deflate(e.subbuttonDeflate);
+				RightCircleRect.Deflate(e.subbuttonDeflate);
+				if ( TEST(CONTAINS_SUBBUTON ))
+				{
+					
+					if ( TEST(SUBBUTTON_RIGHT_END_ROUND ) && !TEST(SUBBUTTON_IS_SHORT_LEFT))
+					{
+						bandRect.right = bandRect.right-bandRect.Height()/2;
+						if ( TEST(SUBBUTTON_IS_FULL))
+						{
+							w.DrawEllipse(RightCircleRect, e.opaqueColor);
+						}
+						else
+						{
+							if (TEST(SUBBUTTON_IS_SHORT_RIGHT))
+							{
+								w.DrawEllipse(RightCircleRect, Null, e.subbuttonBorderWidth, e.opaqueColor);
+							}
+							else
+							{
+								w.DrawArc(RightCircleRect, RightCircleRect.BottomCenter(), RightCircleRect.TopCenter(), e.subbuttonBorderWidth, e.opaqueColor);
+							}
+						}
+					}
+					if (TEST(SUBBUTTON_LEFT_END_ROUND) && !TEST(SUBBUTTON_IS_SHORT_RIGHT))
+					{
+						bandRect.left = bandRect.left+bandRect.Height()/2;
+						if ( TEST(SUBBUTTON_IS_FULL))
+						{
+							w.DrawEllipse(LeftCircleRect, e.opaqueColor);
+						}
+						else
+						{
+							if (TEST(SUBBUTTON_IS_SHORT_LEFT))
+							{
+								w.DrawEllipse(LeftCircleRect, Null, e.subbuttonBorderWidth, e.opaqueColor);
+							}
+							else
+							{
+								w.DrawArc(LeftCircleRect, LeftCircleRect.TopCenter(), LeftCircleRect.BottomCenter(), e.subbuttonBorderWidth, e.opaqueColor);
+							}
+						}
+					}
+					
+					if (!(TEST(SUBBUTTON_IS_SHORT_LEFT) || TEST(SUBBUTTON_IS_SHORT_RIGHT)))
+					{
+						if ( TEST(SUBBUTTON_IS_FULL))
+						{
+							w.DrawRect(bandRect, e.opaqueColor);
+						}
+						else
+						{
+							w.DrawLine(bandRect.TopLeft()   , bandRect.TopRight()   , e.subbuttonBorderWidth, e.opaqueColor);
+							w.DrawLine(bandRect.BottomLeft(), bandRect.BottomRight(), e.subbuttonBorderWidth, e.opaqueColor);
+						}
+					}
+					#if DO_ANTIALIASING
+					dw.DrawImage(rect, ib);
+					#endif
+				}
+				
+				// DRAW BUTTON ICON
+				if ( ! e.buttonIcon.IsEmpty() )
+				{
+					Point imgPos;
+					if (TEST(SUB_ITEM_LEFT))
+					{
+						imgPos = LeftCircleRect.CenterPos(e.buttonIcon.GetSize());
+					}
+					else if(TEST(SUB_ITEM_RIGHT))
+					{
+						imgPos = RightCircleRect.CenterPos(e.buttonIcon.GetSize());
+					}
+					else // draw image at center
+					{
+						imgPos = bandRect.CenterPos(e.buttonIcon.GetSize());
+					}
+					dw.DrawImage( imgPos.x, imgPos.y, e.buttonIcon);
+				}
+				return 1;
+			}
+			default:
+				return Null;
+		}
+	}
+	return Null;
+}
+}
+INITBLOCK {
+	ChLookFn(ButtonStyles::ButtonStyleLookFunction);
+}
+
+

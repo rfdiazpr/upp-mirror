726428eab199a41b8e8b9f59602048dbfe84e4b6
cxl <cxl@05275033-79c2-2956-22f4-0a99e774df92>
2013-06-08 05:59:03 +0000

    .sandbox

---
+++ sandbox/Docking/ToolWin.h
@@ -0,0 +1,55 @@
+class ToolWin : public ParentCtrl {
+public:
+	virtual void MoveBegin();
+	virtual void Moving();
+	virtual void MoveEnd();
+
+	virtual void Paint(Draw& w) { w.DrawRect(GetSize(), Green()); }
+
+private:		
+	struct ToolWinFrame : public TopWindow {
+		virtual void  Paint(Draw& w);
+		virtual void  LeftDown(Point p, dword keyflags);
+		virtual void  MouseMove(Point p, dword keyflags);
+		virtual Image CursorImage(Point p, dword keyflags);
+		virtual void  LeftUp(Point p, dword keyflags);
+		virtual void  Layout();
+		virtual Size  GetMinSize() const;
+		virtual Size  GetMaxSize() const;
+	
+		Point      p0;
+		Rect       rect0;
+		Point      dragdir;
+		ToolButton close;
+	
+		void DoClose();
+		
+		Point GetDir(Point p) const;
+		int   GetTitleCy() const;
+		int   GetBorder() const;
+		Rect  GetMargins() const;
+		Size  AddMargins(Size sz) const;
+		
+		void StartMouseDrag0();
+		
+		ToolWin *client;
+	
+		typedef ToolWinFrame CLASSNAME;
+	
+		void StartMouseDrag();
+		void SetClientRect(Rect r);
+	
+		ToolWinFrame();
+	};
+
+	ToolWinFrame win;
+
+public:
+	void StartMouseDrag()             { win.StartMouseDrag(); }
+	void SetClientRect(const Rect& r) { win.SetClientRect(r); }
+	void PlaceClientRect(Rect r);
+	
+	TopWindow& Win()                  { return win; }
+	
+	ToolWin();
+};

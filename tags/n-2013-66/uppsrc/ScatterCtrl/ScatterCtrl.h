d2e8a3dc9266f0c5b5a3b1f43cd2fe2324734e43
cxl <cxl@05275033-79c2-2956-22f4-0a99e774df92>
2013-10-29 08:24:04 +0000

    .tag n-2013-66

---
+++ tags/n-2013-66/uppsrc/ScatterCtrl/ScatterCtrl.h
@@ -0,0 +1,244 @@
+#ifndef _ScatterCtrl_ScatterCtrl_h
+#define _ScatterCtrl_ScatterCtrl_h
+
+#include "PopUpText.h"
+#include <ScatterDraw/ScatterDraw.h>
+#include <plugin/jpg/jpg.h>
+#include <GridCtrl/GridCtrl.h>
+
+NAMESPACE_UPP
+
+class ArrayCtrlSource : public DataSource {
+private:
+	ArrayCtrl *data;
+	bool useCols;
+	Vector<int> ids;
+	int beginData;
+	int64 numData;
+
+public:
+	ArrayCtrlSource() : data(0), useCols(true), beginData(0), numData(Null) {ids << 0 << 1;}
+	ArrayCtrlSource(ArrayCtrl &data, bool useCols = true, int idX = 0, int idY = 1, int beginData = 0, int numData = Null) : 
+		data(&data), useCols(useCols), beginData(beginData), numData(numData) {
+		Init(data, ids, useCols, beginData, numData);
+	}
+	void Init(ArrayCtrl &_data, Vector<int> &_ids, bool _useCols = true, int _beginData = 0, int _numData = Null) {
+		data = &_data;
+		useCols = _useCols;
+		
+		ids.SetCount(_ids.GetCount());
+		for (int i = 0; i < ids.GetCount(); ++i)
+			ids[i] = _ids[i];
+		beginData = _beginData;
+		numData = _numData;
+		if (IsNull(_numData)) {
+			if (!useCols)
+				numData = data->GetColumnCount() - beginData;
+			else
+				numData = data->GetCount() - beginData;
+		}
+	}
+	void Init(ArrayCtrl &_data, int idY, int idX, bool _useCols = true, int _beginData = 0, int _numData = Null) {
+		Vector<int> ids;
+		ids << idY << idX;
+		Init(_data, ids, _useCols, _beginData, _numData);
+	}
+	virtual inline double y(int64 id)	{return useCols ? data->Get(beginData + int(id), ids[0]) : data->Get(ids[0], beginData + int(id));};
+	virtual inline double x(int64 id) {
+		if (IsNull(ids[1]))
+			return double(id);
+		else
+			return useCols ? data->Get(beginData + int(id), ids[1]) : data->Get(ids[1], beginData + int(id));
+	}
+	virtual inline double xn(int n, int64 id)	{return useCols ? data->Get(beginData + int(id), ids[n]) : data->Get(ids[n], beginData + int(id));}
+	virtual inline int64 GetCount()	{return numData;};
+};
+
+class GridCtrlSource : public DataSource {
+private:
+	GridCtrl *data;
+	bool useCols;
+	Vector<int> ids;
+	int beginData;
+	int64 numData;
+
+public:
+	GridCtrlSource() : data(0), useCols(true), beginData(0), numData(Null)  {ids << 0 << 1;}
+	GridCtrlSource(GridCtrl &data, Vector<int> &_ids, bool useCols = true, int beginData = 0, int numData = Null) : 
+		data(&data), useCols(useCols), beginData(beginData), numData(numData) {
+		Init(data, ids, useCols, beginData, numData);
+	}
+	void Init(GridCtrl &_data, Vector<int> &_ids, bool _useCols = true, int _beginData = 0, int _numData = Null) {
+		data = &_data;
+		useCols = _useCols;
+
+		ids.SetCount(_ids.GetCount());
+		for (int i = 0; i < ids.GetCount(); ++i)
+			ids[i] = _ids[i];
+		beginData = _beginData;
+		numData = _numData;
+		if (IsNull(_numData)) {
+			if (!useCols)
+				numData = data->GetColumnCount() - beginData;
+			else
+				numData = data->GetRowCount() - beginData;
+		}		
+	}
+	void Init(GridCtrl &_data, int idY, int idX, bool _useCols = true, int _beginData = 0, int _numData = Null) {
+		Vector<int> ids;
+		ids << idY << idX;
+		Init(_data, ids, _useCols, _beginData, _numData);
+	}	
+	virtual inline double y(int64 id)	{return useCols ? data->Get(beginData + int(id), ids[0]) : data->Get(ids[0], beginData + int(id));};
+	virtual inline double x(int64 id) {
+		if (IsNull(ids[1]))
+			return double(id);
+		else
+			return useCols ? data->Get(beginData + int(id), ids[1]) : data->Get(ids[1], beginData + int(id));
+	}
+	virtual inline double xn(int n, int64 id)	{return useCols ? data->Get(beginData + int(id), ids[n]) : data->Get(ids[n], beginData + int(id));}
+	virtual inline int64 GetCount()	{return numData;};
+};
+
+class ScatterCtrl : public StaticRect, public ScatterDraw {
+public:
+	typedef ScatterCtrl CLASSNAME;
+	ScatterCtrl();
+	
+	enum MouseAction {NO_ACTION = 0, SCROLL, ZOOM_H_ENL, ZOOM_H_RED, ZOOM_V_ENL, ZOOM_V_RED, SHOW_INFO, CONTEXT_MENU};
+
+	struct MouseBehaviour {
+		bool ctrl;
+		bool alt;
+		bool shift;
+		bool left;
+		bool middle;
+		int middleWheel;
+		bool right;
+		MouseAction action;
+	};
+	
+	#define MAX_MOUSEBEHAVIOR 20
+	
+	bool SetMouseBehavior(MouseBehaviour *_mouseBehavior);
+	ScatterCtrl& ShowContextMenu(bool show = true) 			{showContextMenu = show; return *this;}
+	ScatterCtrl& ShowPropertiesDlg(bool show = true)		{showPropDlg = show; 	 return *this;}
+	ScatterCtrl& SetPopText(const String x, const String y1, const String y2) 	
+															{popTextX = x; popTextY = y1; popTextY2 = y2; return *this;}
+	ScatterCtrl& SetMouseHandling(bool valx = true, bool valy = false);
+	ScatterCtrl& ShowInfo(bool show = true)					{paintInfo = show;		 return *this;}
+	
+#ifdef PLATFORM_WIN32
+	void SaveAsMetafile(const char* file);
+#endif	
+	void SaveToClipboard(bool saveAsMetafile = false);
+	void SaveToFile(String fileName = Null);
+	
+	virtual void Refresh() {Ctrl::Refresh();};
+	virtual Size GetSize() const {return Ctrl::GetSize();};
+	
+	ScatterCtrl& SetColor(const Upp::Color& _color)				{ScatterDraw::SetColor(_color);				return *this;};
+	ScatterCtrl& SetGridColor(const Upp::Color& grid_color)		{ScatterDraw::SetGridColor(grid_color);		return *this;};
+	ScatterCtrl& SetGridWidth(int grid_width) 					{ScatterDraw::SetGridWidth(grid_width); 	return *this;};
+	ScatterCtrl& SetPlotAreaColor(const Upp::Color& p_a_color)	{ScatterDraw::SetPlotAreaColor(p_a_color); 	return *this;};
+	ScatterCtrl& SetLegendWidth(int width)						{ScatterDraw::SetLegendWidth(width); 		return *this;};
+	int GetLegendWidth()										{return ScatterDraw::GetLegendWidth();};
+	ScatterCtrl& SetAxisColor(const Upp::Color& axis_color)		{ScatterDraw::SetAxisColor(axis_color);		return *this;};
+	ScatterCtrl& SetAxisWidth(int axis_width)					{ScatterDraw::SetAxisWidth(axis_width);		return *this;};
+	ScatterCtrl& SetTitle(const String& title)		 			{ScatterDraw::SetTitle(title); 				return *this;};
+	ScatterCtrl& SetTitleFont(const Font& fontTitle) 			{ScatterDraw::SetTitleFont(fontTitle); 		return *this;};
+	ScatterCtrl& SetTitleColor(const Upp::Color& colorTitle)	{ScatterDraw::SetTitleColor(colorTitle);	return *this;};
+	ScatterCtrl& SetLabelsFont(const Font& fontLabels) 			{ScatterDraw::SetLabelsFont(fontLabels); 	return *this;};
+	ScatterCtrl& SetLabelsColor(const Upp::Color& colorLabels)	{ScatterDraw::SetLabelsColor(colorLabels);	return *this;};
+	ScatterCtrl& SetLabelX(const String& xLabel)				{ScatterDraw::SetLabelX(xLabel); 			return *this;};
+	ScatterCtrl& SetLabelY(const String& yLabel)				{ScatterDraw::SetLabelY(yLabel); 			return *this;};
+	ScatterCtrl& SetLabelY2(const String& yLabel)				{ScatterDraw::SetLabelY2(yLabel); 			return *this;};
+	ScatterCtrl& SetPlotAreaMargin(int hLeft, int hRight, int vTop, int vBottom)
+																{ScatterDraw::SetPlotAreaMargin(hLeft, hRight, vTop, vBottom); return *this;};
+	ScatterCtrl& SetPlotAreaLeftMargin(int margin)				{ScatterDraw::SetPlotAreaLeftMargin(margin);return *this;};
+	ScatterCtrl& SetPlotAreaTopMargin(int margin)				{ScatterDraw::SetPlotAreaTopMargin(margin);	return *this;};
+	ScatterCtrl& SetPlotAreaRightMargin(int margin)				{ScatterDraw::SetPlotAreaRightMargin(margin);return *this;};
+	ScatterCtrl& SetPlotAreaBottomMargin(int margin)			{ScatterDraw::SetPlotAreaBottomMargin(margin);return *this;};
+	
+	ScatterCtrl& ShowLegend(bool show = true) 					{ScatterDraw::ShowLegend(show); 			return *this;}
+	bool GetShowLegend() 										{return ScatterDraw::GetShowLegend();}
+	
+	using ScatterDraw::AddSeries; 
+	ScatterCtrl &AddSeries(ArrayCtrl &data, bool useCols = true, int idX = 0, int idY = 1, int idZ = 2, int beginData = 0, int numData = Null);
+	using ScatterDraw::InsertSeries; 
+	void InsertSeries(int id, ArrayCtrl &data, bool useCols = true, int idX = 0, int idY = 1, int idZ = 2, int beginData = 0, int numData = Null);	
+	
+	ScatterCtrl& SetCopyRatio(int ratio)				{copyRatio = ratio; return *this;}
+	int GetCopyRatio()									{return copyRatio;}
+	
+	ScatterCtrl& SetMaxRefreshTime(int _maxRefresh_ms) 	{maxRefresh_ms = _maxRefresh_ms; return *this;}
+	int GetMaxRefreshTime() 							{return maxRefresh_ms;}
+	
+private:
+	bool paintInfo;
+	Point clickPoint;
+	PopUpInfo popText;
+	String popTextX, popTextY, popTextY2;
+	Point popLT, popRB;
+	const Point offset;
+	int copyRatio;
+
+	int butDownX, butDownY;
+	bool isScrolling, isLabelPopUp;
+	
+	bool mouseHandlingX, mouseHandlingY;
+	bool showContextMenu;
+	bool showPropDlg;
+	
+	int lastRefresh_ms;
+	dword lastRefresh0_ms;
+	int maxRefresh_ms;
+	
+	bool highlighting;
+	
+	MouseBehaviour *mouseBehavior;
+	
+	void ProcessPopUp(const Point & pt);
+
+	virtual void Paint(Draw& w);
+	virtual void LeftDown(Point, dword);
+	virtual void LeftUp(Point, dword);
+	virtual void MiddleDown(Point, dword);
+	virtual void MouseMove(Point, dword);
+	virtual void MiddleUp(Point, dword);
+	virtual void RightDown(Point, dword);
+	virtual void RightUp(Point, dword);
+	virtual void MouseLeave();
+	virtual void MouseWheel(Point, int zdelta, dword);
+	
+	void DoMouseAction(bool down, Point pt, MouseAction action, int value);
+	void ProcessMouse(bool down, Point &pt, bool ctrl, bool alt, bool shift, bool left, bool middle, int middleWheel, bool right); 
+	void LabelPopUp(bool down, Point &pt); 
+	void Scrolling(bool down, Point &pt, bool isOut = false);
+	void MouseZoom(int zdelta, bool hor, bool ver);
+	
+	void ContextMenu(Bar& bar);
+	void DoShowEditDlg();
+	
+	virtual Image CursorImage(Point p, dword keyflags);
+	
+	template <class T>
+	void SetDrawing(T& w, const Size &size, int scale);	
+	void TimerCallback();	
+};
+
+template <class T>
+void ScatterCtrl::SetDrawing(T& w, const Size &size, int scale) {
+	ScatterDraw::SetDrawing(w, size, scale, true);
+	if (!IsNull(popLT) && popLT != popRB) {
+		DrawVArrow(w, popLT.x, popLT.y, popLT.x, popRB.y, 1, 4, 15, SColorHighlight());
+		DrawHArrow(w, popLT.x, popRB.y, popRB.x, popRB.y, 1, 4, 15, SColorHighlight());
+		Ctrl::Refresh(min(popLT.x-4, popRB.x-4), min(popLT.y-4, popRB.y-4), 
+					  abs(popRB.x-popLT.x) + 9, abs(popRB.y-popLT.y) + 9);
+	}
+	ClipEnd(w);
+}
+
+END_UPP_NAMESPACE
+
+#endif
---
+++ tags/n-2013-66/uppsrc/ide/Common/ComDlg.cpp
@@ -0,0 +1,184 @@
+#include "Common.h"
+
+#define IMAGECLASS IdeCommonImg
+#define IMAGEFILE  <ide/Common/common.iml>
+#include <Draw/iml_source.h>
+
+void IdeFileIcon0(bool dir, const String& filename, Image& img)
+{
+	if(dir) return;
+	String ext = ToLower(GetFileExt(filename));
+	if(ext == ".h" || ext == ".hpp" || ext == ".hh" || ext == ".hxx")
+		img = IdeCommonImg::Header();
+	for(int i = 0; i < GetIdeModuleCount(); i++) {
+		Image m = GetIdeModule(i).FileIcon(filename);
+		if(!IsNull(m)) {
+			img = m;
+			break;
+		}
+	}
+	if(ext == ".html")
+		img = IdeCommonImg::html();
+	if(ext == ".css")
+		img = IdeCommonImg::css();
+	if(ext == ".witz")
+		img = IdeCommonImg::witz();
+	if(ext == ".js")
+		img = IdeCommonImg::js();
+	if(ext == ".usc")
+		img = IdeCommonImg::Script();
+	if(ext == ".lng" || ext == ".lngj" || ext == ".t" || ext == ".jt")
+		img = IdeCommonImg::Language();
+	if(ext == ".icpp")
+		img = IdeCommonImg::ISource();
+	if(ext == ".c" || ext == ".cpp" || ext == ".cc" || ext == ".cxx"
+	               || ext == ".m" || ext == ".mm")  //should later have diff img?
+		img = IdeCommonImg::Source();
+	if(ext == ".sch")
+		img = IdeCommonImg::Sch();
+	if(ext == ".ddl")
+		img = IdeCommonImg::Ddl();
+	if(ext == ".sql")
+		img = IdeCommonImg::Sql();
+	if(filename == "Copying")
+		img = IdeCommonImg::License();
+	if(filename == "main.conf")
+		img = IdeCommonImg::MainConf();
+}
+
+struct sImageAdd : ImageMaker {
+	Image i1, i2;
+
+	virtual String Key() const;
+	virtual Image Make() const;
+};
+
+String sImageAdd::Key() const
+{
+	int64 a[2];
+	a[0] = i1.GetSerialId();
+	a[1] = i2.GetSerialId();
+	return String((const char *)&a, 2 * sizeof(int64));
+}
+
+Image sImageAdd::Make() const
+{
+	Image dest = i1;
+	Over(dest, Point(0, 0), i2, i2.GetSize());
+	return dest;
+}
+
+Image ImageOver(const Image& back, const Image& over)
+{
+	sImageAdd h;
+	h.i1 = back;
+	h.i2 = over;
+	return MakeImage(h);
+}
+
+Image IdeFileImage(const String& filename, bool fast, bool include_path)
+{
+	Image img = CtrlImg::File();
+	IdeFileIcon0(false, filename, img);
+	if(fast)
+		img = ImageOver(img, IdeCommonImg::Fast());
+	if(include_path)
+		img = ImageOver(img, IdeCommonImg::IncludePath());
+	return img;
+}
+
+void IdeFileIcon(bool dir, const String& filename, Image& img)
+{
+	IdeFileIcon0(dir, filename, img);
+}
+
+void IdeFs(FileSel& fs) {
+	fs.WhenIcon = callback(IdeFileIcon);
+	fs.AllFilesType();
+	fs.Multi();
+	fs.NoAsking();
+	fs.ReadOnlyOption();
+}
+
+void SourceFs(FileSel& fs) {
+	fs.Type("C++ files (*.cpp *.h *.hpp *.c *.C *.cc *.cxx *.icpp)", "*.cpp *.h *.hpp *.c *.C *.cc *.cxx *.icpp");
+	fs.Type("Layout files (*.lay)", "*.lay");
+	fs.Type("Image files (*.iml)", "*.iml");
+	fs.Type("Language files (*.lng)", "*.lng");
+	fs.Type("Web development files (*.html *.js *.css *.witz)", "*.html *.js *.css *.witz");
+	fs.Type("Other special files (*.sch *.usc *.rc *.brc *.upt)", "*.sch *.usc *.rc *.brc *.upt");
+	String mask = "*.cpp *.h *.hpp *.c *.C *.cc *.cxx *.icpp *.lay *.iml *.lng *.sch *.usc *.rc *.brc *.upt *.html *.js *.css *.witz";
+	fs.Type("All source files (" + mask + ")", mask);
+	IdeFs(fs);
+}
+
+FileSel& AnySourceFs() {
+	static FileSel *fsp;
+	if(!fsp) {
+		static FileSel fs;
+		SourceFs(fs);
+		fsp = &fs;
+	}
+	fsp->Multi();
+	return *fsp;
+}
+
+FileSel& AnyPackageFs() {
+	static FileSel fs;
+	static bool b;
+	if(!b) {
+		fs.Type("Ultimate++ package (*.upp)", "*.upp");
+		fs.AllFilesType();
+		b = true;
+	}
+	return fs;
+}
+
+FileSel& BasedSourceFs() {
+	static FileSel *fsp;
+	if(!fsp) {
+		static FileSel fs;
+		SourceFs(fs);
+		fsp = &fs;
+	}
+	fsp->Multi();
+	return *fsp;
+}
+
+FileSel& OutputFs() {
+	static FileSel *fsp;
+	if(!fsp) {
+		static FileSel fs;
+		fs.AllFilesType();
+		fs.Type("Various (*.log *.map *.ini *.sql)", "*.log *.map *.ini *.sql");
+		fs.Type("Log files (*.log)", "*.log");
+		fs.Type("Map files (*.map)", "*.map");
+		fs.Type("Ini files (*.ini)", "*.ini");
+		fs.Type("SQL scripts (*.sql)", "*.sql");
+		IdeFs(fs);
+		fsp = &fs;
+	}
+	fsp->Multi();
+	return *fsp;
+}
+
+void ShellOpenFolder(const String& dir)
+{
+	#if defined(PLATFORM_WIN32)
+		LaunchWebBrowser(dir);
+	#elif __APPLE__
+		String tempDir = dir;
+		tempDir.Replace(" ", "\\ ");
+
+		IGNORE_RESULT(
+			system("open " + dir + " &")
+		);
+	#else
+		String tempDir = dir;
+		tempDir.Replace(" ", "\\ ");
+		
+		IGNORE_RESULT(
+			system("xdg-open " + tempDir + " &")
+		);
+	#endif
+}

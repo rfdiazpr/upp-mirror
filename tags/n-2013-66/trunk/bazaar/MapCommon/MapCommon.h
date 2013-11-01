0cda7d8c943159dceedbfb4e2c60827a239e20bd
cxl <cxl@05275033-79c2-2956-22f4-0a99e774df92>
2013-11-01 09:04:32 +0000

    .removing wrong tag

--- tags/n-2013-66/trunk/bazaar/MapCommon/MapCommon.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAP_COMMON__MAP_COMMON_H
-#define MAP_COMMON__MAP_COMMON_H
-
-#include "IMapRender.h"
-
-#include "ButtonItem.h"
-#include "PolygonItem.h"
-
-#endif
--- tags/n-2013-66/trunk/bazaar/UnitTestTest/UnitTestTest.cpp
+++
@@ -1,9 +0,0 @@
-#include <Core/Core.h>
-#include <UnitTest++/UnitTest++.h>
-#include <UnitTest++/TestReporterStdout.h>
-
-CONSOLE_APP_MAIN
-{
-	UPP::SetExitCode( UnitTest::RunAllTests() );
-}
-

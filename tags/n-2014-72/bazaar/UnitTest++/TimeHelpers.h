737226784743f91acf71f26a1ed8a19d394652e2
cxl <cxl@05275033-79c2-2956-22f4-0a99e774df92>
2014-01-31 14:36:02 +0000

    .removing tag n-2014-72

--- tags/n-2014-72/bazaar/UnitTest++/TimeHelpers.h
+++
@@ -1,8 +0,0 @@
-#include "Config.h"
-
-//#if !defined PLATFORM_WIN32
-#ifndef flagWIN32
-    #include "Posix/TimeHelpersPosix.h"
-#else
-    #include "Win32/TimeHelpersWin32.h"
-#endif
--- tags/n-2014-72/uppsrc/ScatterCtrl/src.tpp/all.i
+++
@@ -1,8 +0,0 @@
-TOPIC("MouseBehavior$en-us")
-#include "MouseBehavior$en-us.tppi"
-END_TOPIC
-
-TOPIC("ScatterCtrl$en-us")
-#include "ScatterCtrl$en-us.tppi"
-END_TOPIC
-

7b9900ff50c7c20732285ca0445adc82371a3c97
cxl <cxl@05275033-79c2-2956-22f4-0a99e774df92>
2014-06-06 13:38:43 +0000

    .tag n-2014-81

---
+++ tags/n-2014-81/uppsrc/Core/CoWork.h
@@ -0,0 +1,55 @@
+#ifdef _MULTITHREADED
+
+class CoWork : NoCopy {
+	typedef StaticCriticalSection Lock;
+
+	struct MJob : Moveable<MJob> {
+		Callback cb;
+		CoWork  *work;
+	};
+
+	struct Pool {
+		Vector<MJob>    jobs;
+		int             waiting_threads;
+		Array<Thread>   threads;
+
+		CriticalSection lock;
+		Semaphore       waitforjob;
+
+		Pool();
+		~Pool();
+
+		static bool DoJob();
+		static void ThreadRun(int tno);
+	};
+	
+	friend struct Pool;
+
+	static Pool& pool();
+
+	Semaphore waitforfinish;
+	int       todo;
+
+public:
+	void     Do(Callback cb);
+	CoWork&  operator&(Callback cb) { Do(cb); return *this; }
+
+	void Finish();
+	
+	bool IsFinished();
+
+	CoWork();
+	~CoWork();
+};
+
+#else
+
+class CoWork : NoCopy {
+public:
+	void     Do(Callback cb)        { cb(); }
+	CoWork&  operator&(Callback cb) { cb(); return *this; }
+	void     Finish()               {}
+	bool     IsFinished()           { return true; }
+};
+
+#endif

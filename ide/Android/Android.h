4634c56e13d77317f3923208717d7e9b06363cc2
klugier <klugier@05275033-79c2-2956-22f4-0a99e774df92>
2015-05-08 20:53:01 +0000

    .android_builder sdk class can now detects android platforms and build tools releases.

--- ide/Android/Android.h
+++ ide/Android/Android.h
@@ -11,40 +11,58 @@ public:
 	AndroidSDK(const String& path);
 	virtual ~AndroidSDK();
 	
+	// TODO: Method may be too high level, but for testing purpose it should be OK.
+	void DeducePathReleatedValues();
+	
 	bool CheckIntegrity() const;
 	
-	Vector<String> GetAVDNames() const;
+	Vector<String> FindPlatforms() const;
+	Vector<String> FindBuildToolsReleases() const;
+	Vector<String> FindAVDNames() const;
+	String         FindDefaultPlatform() const;
+	String         FindDefaultBuildToolsRelease() const;
 	
 public:
 	String BuildToolsDir() const;
-	String PlatformDir() const;
+	String PlatformsDir() const;
+	String ConcreteBuildToolsDir() const;
+	String ConcretePlatformDir() const;
 	String PlatformToolsDir() const;
 	String ToolsDir() const;
 	
-	String AaptPath() const       { return BuildToolsDir() + DIR_SEPS + "aapt" + ExecutableExt(); }
-	String DxPath() const         { return BuildToolsDir() + DIR_SEPS + "dx" + ExecutableExt(); }
-	String ZipalignPath() const   { return BuildToolsDir() + DIR_SEPS + "zipalign" + ExecutableExt(); }
-	String AndroidJarPath() const { return PlatformDir() + DIR_SEPS + "android.jar"; }
+	String AaptPath() const       { return ConcreteBuildToolsDir() + DIR_SEPS + "aapt" + ExecutableExt(); }
+	String DxPath() const         { return ConcreteBuildToolsDir() + DIR_SEPS + "dx" + ExecutableExt(); }
+	String ZipalignPath() const   { return ConcreteBuildToolsDir() + DIR_SEPS + "zipalign" + ExecutableExt(); }
+	String AndroidJarPath() const { return ConcretePlatformDir() + DIR_SEPS + "android.jar"; }
 	String AdbPath() const        { return PlatformToolsDir() + DIR_SEPS + "adb" + ExecutableExt(); }
 	String AndroidPath() const    { return ToolsDir() + DIR_SEPS + "android" + ExecutableExt(); }
 	String EmulatorPath() const   { return ToolsDir() + DIR_SEPS + "emulator" + ExecutableExt(); }
 	
 public:
-	String GetPath() const { return this->path; }
+	String GetPath() const              { return this->path; }
+	String GetPlatform() const          { return this->platform; }
+	String GetBuildToolsRelease() const { return this->buildToolsRelease; }
 	
-	void SetPath(const String& path) { this->path = path; }
+	void SetPath(const String& path)                           { this->path = path; }
+	void SetPlatform(const String& platform)                   { this->platform = platform; }
+	void SetBuildToolsRelease(const String& buildToolsRelease) { this->buildToolsRelease = buildToolsRelease; }
 	
 protected:
 	String ExecutableExt() const;
 	
 private:
 	String path;
+	String platform;
+	String buildToolsRelease;
 };
 
 class AndroidNDK {
 public:
 	AndroidNDK();
 	AndroidNDK(const String& path);
+	virtual ~AndroidNDK();
+	
+	bool CheckIntegrity() const;
 	
 public:
 	// TODO: MS Windows exe.
